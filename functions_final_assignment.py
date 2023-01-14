87636147343b454ad0b1ad2cebecbe80015e3e76
Tim Niemeijer <ti.niemeijer@st.hanze.nl>
2023-01-15 00:01:37 +0100

    Added  a plot to assess the distribution of the data

--- functions_final_assignment.py
+++ functions_final_assignment.py
@@ -4,14 +4,16 @@ Functions python file to keep the Jupyter notebook clean.
 
 import yaml
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.stats import norm, iqr
 
 
 def yaml_config():
     """
-    Open yaml config file in the same directory as the main script.
+    Opens config.yaml in the same directory as the main script.
     ---------------------
     Output: config yaml as a python library.
-
     """
     with open(file="config.yaml",mode='r',encoding="utf8") as conf_file:
         config = yaml.safe_load(conf_file)
@@ -20,6 +22,8 @@ def yaml_config():
 def load_concat_df(list_of_paths):
     """
     Use a list of paths, load csv and concaternate to a single dataframe.
+    >>> load_concat_df(["//path", "//path"])
+
     ---------------------
     Input: List with paths
     Output: Concaternated Dataframe
@@ -45,3 +49,26 @@ def make_stn_dict(stn_description_file):
                 stn_number = stn_file.readline()[14:17]
                 stn_coordinate_dict.update({stn_number:coordinates})
     return stn_coordinate_dict
+
+
+def hist_robust_norm(dataframe):
+    """
+    Histogram with normal distribution using the robust method.
+    Borrowed pieces of code from the DS1 statistics functions from Emile Apol.
+    """
+    mu = np.mean(dataframe)
+    sigma = iqr(dataframe)/1.349
+
+    x = np.linspace(np.min(dataframe), np.max(dataframe), 501)
+    rv = np.array([norm.pdf(xi, loc = mu, scale = sigma) for xi in x])
+
+    print(f'mu (robust) = {mu:.5}, sigma (robust) = {sigma:.5}')
+    plt.hist(dataframe, density=True,
+     bins='auto', alpha=1, rwidth=1, label="experimental")
+    plt.plot(x, rv, 'r', label='Normal approximation')
+    plt.axvline(mu, color="red", label="Mu")
+    plt.ylabel("Probability")
+    plt.xlabel("Precipitation(x0.1mm)")
+    plt.title("Histogram normal distribution (robust)")
+    plt.legend()
+    plt.show()
