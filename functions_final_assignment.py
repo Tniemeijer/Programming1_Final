4fce868c736e6119520e65458802a4a3d74015a5
Tim Niemeijer <ti.niemeijer@st.hanze.nl>
2023-02-05 13:56:39 +0100

    added weights to the STN/provinces midpoint dataframe

--- functions_final_assignment.py
+++ functions_final_assignment.py
@@ -9,6 +9,7 @@ import numpy as np
 from scipy.stats import norm, gamma, iqr
 import re
 
+plt.style.use("seaborn")
 
 def yaml_config():
     """
@@ -95,26 +96,105 @@ def hist_robust_dist(dataframe, distribution = 'norm'):
     plt.legend()
     plt.show()
 
-
-def validate_precipitation(x,lower,upper):
+def DS_Q_Q_Plot(y, est = 'robust', **kwargs):
     """
-    Function checks if x (input precipitation value) is higher or lower than
-    the upper and lower bounds.
-
-    >>> validate_precipitation(x=200,lower=100,upper=300)
-
-    ---------------------
-    Input: x (numerical value, either int or float)
-
-    Output: string with either "wet","dry" or "normal"
-
+    *
+    Function DS_Q_Q_Plot(y, est = 'robust', **kwargs)
+    
+       This function makes a normal quantile-quantile plot (Q-Q-plot), also known
+       as a probability plot, to visually check whether data follow a normal distribution.
+    
+    Requires:            - 
+    
+    Arguments:
+      y                  data array
+      est                Estimation method for normal parameters mu and sigma:
+                         either 'robust' (default), or 'ML' (Maximum Likelihood),
+                         or 'preset' (given values)
+      N.B. If est='preset' than the *optional* parameters mu, sigma must be provided:
+      mu                 preset value of mu
+      sigma              preset value of sigma
+      
+    Returns:
+      Estimated mu, sigma, n, and expected number of datapoints outside CI in Q-Q-plot.
+      Q-Q-plot
+      
+    Author:            M.E.F. Apol
+    Date:              2020-01-06, revision 2022-08-30
     """
-    if x < lower:
-        return "dry"
-    elif x > upper:
-        return "wet"
+    
+    import numpy as np
+    from scipy.stats import iqr # iqr is the Interquartile Range function
+    import matplotlib.pyplot as plt
+    
+    # First, get the optional arguments mu and sigma:
+    mu_0 = kwargs.get('mu', None)
+    sigma_0 = kwargs.get('sigma', None)
+    
+    n = len(y)
+    
+    # Calculate order statistic:
+    y_os = np.sort(y)
+  
+    # Estimates of mu and sigma:
+    # ML estimates:
+    mu_ML = np.mean(y)
+    sigma2_ML = np.var(y)
+    sigma_ML = np.std(y) # biased estimate
+    s2 = np.var(y, ddof=1)
+    s = np.std(y, ddof=1) # unbiased estimate
+    # Robust estimates:
+    mu_R = np.median(y)
+    sigma_R = iqr(y)/1.349
+
+    # Assign values of mu and sigma for z-transform:
+    if est == 'ML':
+        mu, sigma = mu_ML, s
+    elif est == 'robust':
+        mu, sigma = mu_R, sigma_R
+    elif est == 'preset':
+        mu, sigma = mu_0, sigma_0
     else:
-        return "normal"
+        print('Wrong estimation method chosen!')
+        return()
+        
+    print('Estimation method: ' + est)
+    print('n = {:d}, mu = {:.4g}, sigma = {:.4g}'.format(n, mu,sigma))
+    
+    # Expected number of deviations (95% confidence level):
+    n_dev = np.round(0.05*n)
+    
+    print('Expected number of data outside CI: {:.0f}'.format(n_dev))
+         
+    # Perform z-transform: sample quantiles z.i
+    z_i = (y_os - mu)/sigma
+
+    # Calculate cumulative probabilities p.i:
+    i = np.array(range(n)) + 1
+    p_i = (i - 0.5)/n
+
+    # Calculate theoretical quantiles z.(i):
+    from scipy.stats import norm
+    z_th = norm.ppf(p_i, 0, 1)
+
+    # Calculate SE or theoretical quantiles:
+    SE_z_th = (1/norm.pdf(z_th, 0, 1)) * np.sqrt((p_i * (1 - p_i)) / n)
+
+    # Calculate 95% CI of diagonal line:
+    CI_upper = z_th + 1.96 * SE_z_th
+    CI_lower = z_th - 1.96 * SE_z_th
+
+    # Make Q-Q plot:
+    plt.plot(z_th, z_i, 'o', color='k', label='experimental data')
+    plt.plot(z_th, z_th, '--', color='r', label='normal line')
+    plt.plot(z_th, CI_upper, '--', color='b', label='95% CI')
+    plt.plot(z_th, CI_lower, '--', color='b')
+    plt.xlabel('Theoretical quantiles, $z_{(i)}$')
+    plt.ylabel('Sample quantiles, $z_i$')
+    plt.title('Q-Q plot (' + est + ')')
+    plt.legend(loc='best')
+    plt.show()
+    pass;
 
 def read_geojson(path):
     """
@@ -152,10 +232,12 @@ def get_coordinates_df(nested_list_coord):
 
 def get_centerpoint(dataframe_coordinates):
     """
-    Source: https://stackoverflow.com/questions/4355894/how-to-get-center-of-set-of-points-using-python
+    Source: 
+    https://stackoverflow.com/questions/4355894/how-to-get-center-of-set-of-points-using-python
     Function calculates the centroid of the inserted.
 
     """
+    
     data = get_coordinates_df(dataframe_coordinates)
 
     x = data.x.to_numpy() 
@@ -165,19 +247,21 @@ def get_centerpoint(dataframe_coordinates):
 
     N = range(len(data)-1)
     M = np.array([(x[i]-x[i+1])*(y[i]+y[i+1])/2 for i in N]) #Area of each trapezoid
-    My = np.array([(x[i]+x[i+1])/2 for i in N])*M #Moment of area (area*distance to centroid) with respect to the Y axis of each trapezoid
-    Mx = np.array([(y[i]+y[i+1])/4 for i in N])*M #Moment of area (area*distance to centroid) with respect to the X axis of each trapezoid
+    My = np.array([(x[i]+x[i+1])/2 for i in N])*M 
+    #Moment of area (area*distance to centroid) with respect to the Y axis of each trapezoid
+    Mx = np.array([(y[i]+y[i+1])/4 for i in N])*M 
+    #Moment of area (area*distance to centroid) with respect to the X axis of each trapezoid
     X = sum(My)/sum(M)
     Y = sum(Mx)/sum(M)
     centroid = [X,Y]
     points_ave = data.mean(axis=0)
 
-    plt.plot(data.x, data.y, 'r',marker='.',markeredgecolor='black', markersize=3)
+    plt.plot(data.x, data.y, 'grey',marker='.',markeredgecolor='black', markersize=3)
     plt.plot(*centroid, 'blue', marker='o',markeredgecolor='black', markersize=7)
     plt.plot(*points_ave, 'green', marker='o',markeredgecolor='black', markersize=7)
-    plt.axis('equal')
-    plt.xlim((4, 6))
-    plt.ylim((51,53))
+    plt.axis('equal');plt.xlim((3, 7));plt.ylim((50.5,54))
+    plt.title("Plotted map of the provinces and centroids")
+    plt.xlabel('latitude');plt.ylabel('longitude')
     plt.legend(['line','Centroid','Average of points'])
     return [Y,X]
 
